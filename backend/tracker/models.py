e26713bea4d82c4cfc715267299eeb210e752cb9
Gokul <gokulg6963@gmail.com>
2025-05-04 00:20:35 +0530

    Added Model for User

--- backend/tracker/models.py
+++ backend/tracker/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# UserProfile model
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    minimum_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    enable_minimum_balance = models.BooleanField(default=False)
+
